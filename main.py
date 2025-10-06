a42f806ba5b245e6c19dc92b57ca71efbcde2fc0
AcidicSoil <claytonbivens1@gmail.com>
2025-10-06 11:00:33 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+# main.py - convenience entry point for running the teaching analyzer module
+from dspy_file.analyze_file_cli import main
+
+if __name__ == "__main__":
+    raise SystemExit(main())
+how
